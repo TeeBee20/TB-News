8d21ebf763eb576187e99398723b3e657469c0a7
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-12 16:43:56 +0100

    add get path for /api in app

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,7 +3,6 @@ const apiRouter = require("express").Router();
 const articlesRouter = require("../routers/articles-router");
 
 apiRouter.use("/articles", articlesRouter);
-apiRouter.get(getEndpoints);
 apiRouter.get("/topics", getTopics);
 apiRouter.get("/seed", seedController);
 
