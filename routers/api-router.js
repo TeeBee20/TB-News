1c0d3be9bd5d57459f7683b74ef7566ebc9f9b3f
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-12 16:41:58 +0100

    remove end path for /api router

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,7 +3,7 @@ const apiRouter = require("express").Router();
 const articlesRouter = require("../routers/articles-router");
 
 apiRouter.use("/articles", articlesRouter);
-apiRouter.get("", getEndpoints);
+apiRouter.get(getEndpoints);
 apiRouter.get("/topics", getTopics);
 apiRouter.get("/seed", seedController);
 
