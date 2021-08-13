7f50d58154d65cc9d985b89173960f64e107fb8a
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-08-13 15:50:53 +0100

    add previous code to main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -564,14 +564,14 @@ describe("GET - /api", () => {
       });
   });
 });
-describe("* - invalid file path", () => {
-  it('404: returns "not found" error for path that does not exist', () => {
-    return request(app)
-      .get("/not-a-path")
-      .expect(404)
-      .then((response) => {
-        const { body } = response;
-        expect(body.msg).toBe("not found");
-      });
-  });
-});
+// describe("* - invalid file path", () => {
+//   it('404: returns "not found" error for path that does not exist', () => {
+//     return request(app)
+//       .get("/not-a-path")
+//       .expect(404)
+//       .then((response) => {
+//         const { body } = response;
+//         expect(body.msg).toBe("not found");
+//       });
+//   });
+// });
